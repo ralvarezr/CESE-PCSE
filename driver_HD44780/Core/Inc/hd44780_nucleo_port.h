c62cd9fb141c60557177b0aea5a8a4c57ed90013
Ricardo Alvarez <ricardo.alvarezr94@gmail.com>
2022-04-24 19:38:52 -0300

    Se agregan las funciones en los archivos port

--- driver_HD44780/Core/Inc/hd44780_nucleo_port.h
+++ driver_HD44780/Core/Inc/hd44780_nucleo_port.h
@@ -8,6 +8,12 @@
 #ifndef INC_HD44780_NUCLEO_PORT_H_
 #define INC_HD44780_NUCLEO_PORT_H_
 
+#include "stm32f4xx_hal.h"
+#include <stdint.h>
+
+
+void i2c_transmit_nucleo_port(I2C_HandleTypeDef *hi2c, uint16_t address, uint8_t data);
+void delay_nucleo_port(uint32_t ms);
 
 
 #endif /* INC_HD44780_NUCLEO_PORT_H_ */
--- driver_HD44780/Core/Src/hd44780_nucleo_port.c
+++ driver_HD44780/Core/Src/hd44780_nucleo_port.c
@@ -5,4 +5,32 @@
  *      Author: Ricardo
  */
 
+#include "hd44780_nucleo_port.h"
 
+
+/**
+ * @brief Esta funcion transmite los datos a través del I2C de la placa nucleo.
+ * 
+ * @param hi2c Handler del I2C de la placa nucleo.
+ * @param address Dirección del esclavo.
+ * @param data Datos a enviar.
+ * 
+ * @return None.
+ */
+void i2c_transmit_nucleo_port(I2C_HandleTypeDef *hi2c, uint16_t address, uint8_t data) {
+
+        HAL_I2C_Master_Transmit(hi2c, address, &data, (uint16_t)1, (uint32_t)10);
+}
+
+/**
+ * @brief Esta funcion implementar un delay en milisegundos.
+ * 
+ * @param ms Los milisegundos para realizar el delay.
+ * 
+ * @return None.
+ * 
+ * @see HAL_Delay.
+ */
+void delay_nucleo_port(uint32_t ms) {
+        HAL_Delay(ms);
+}
